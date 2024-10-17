d6059a3edb8d2605131d794fe237a4ae071cd7b1
SydenMorton <138412155+SydenMorton@users.noreply.github.com>
2024-10-17 17:42:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+﻿function addComment() {
+    const input = document.getElementById('comment-input');
+    const commentList = document.getElementById('comment-list');
+    const commentText = input.value.trim();
+
+    if (commentText) {
+        const comment = document.createElement('div');
+        comment.textContent = commentText;
+        commentList.appendChild(comment);
+        input.value = ''; // очистка поля ввода
+    } else {
+        alert('Пожалуйста, введите комментарий!');
+    }
+}
+
+function likeAnime(button) {
+    const countSpan = button.querySelector('span');
+    let count = parseInt(countSpan.textContent);
+    countSpan.textContent = count + 1;
+}
